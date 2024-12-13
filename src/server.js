7cc61d3d6fbd890ab2fb78935b30aa213d556ee5
Emilio <emilio.pg95@gmail.com>
2024-12-13 16:44:55 -0600

    ✨ Integrate Sequelize synchronization before starting the server to ensure database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 // Load environment variables from .env file
 require('dotenv').config();
+const app = require('./app');
 
 // filepath: /src/server.js
 const app = require('./app');
+const sequelize = require('./config/db');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running in ${PORT} bro`);        
+    });
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
 });
