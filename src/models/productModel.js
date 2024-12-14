f8f31caf1fc5dbe33dc67bfef5507183df8b88bd
Emilio <emilio.pg95@gmail.com>
2024-12-14 09:25:44 -0600

    ✨ Create productModel.js for managing product data with Sequelize

---
+++ src/models/productModel.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db'); // Sequelize instance
+
+const Product = sequelize.define('Product', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+}, {
+    timestamps: false, // Disable automatic timestamps
+});
+
+module.exports = Product;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 // Load environment variables from .env file
 require('dotenv').config();
 const app = require('./app');
-
-// filepath: /src/server.js
-const app = require('./app');
 const sequelize = require('./config/db');
+
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
