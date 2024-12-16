c9d0bde70aae8010adb460613d68687fc46d5ed7
Emilio <emilio.pg95@gmail.com>
2024-12-16 16:03:24 -0600

    ➕ Add route imports and setup for product, user, order, cart, and authentication APIs

--- src/app.js
+++ src/app.js
@@ -5,6 +5,23 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+//Import and use routes
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const orderItemRoutes = require('./routes/orderItemRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const cartItemRoutes = require('./routes/cartItemRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/orders', orderItemRoutes);
+app.use('/api/carts', cartRoutes);
+app.use('/api/carts', cartItemRoutes);
+app.use('/api/auth', authRoutes);
+
 // Basic route
 app.get('/', (req, res) => {
     res.send('Server is running, my dude.');
