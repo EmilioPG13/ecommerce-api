4420106faa1fe1649eb2380894f576e9f72bb9be
Emilio <emilio.pg95@gmail.com>
2025-03-11 19:15:33 -0600

    ➕ Add Home component for the main page layout and navigation

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/Home.css'
+
+const Home = () => {
+    return (
+        <div className='home-page'>
+            <div className='hero'>
+                <div className='container'>
+                    <h1>Welcome to my E-Commerce Store</h1>
+                    <p>Discover amazing products at great prices.</p>
+                    <Link to='/products' className='btn btn-primary'>Shop Now!</Link>
+                </div>
+            </div>
+
+            <section className='featured-products'>
+                <div className='container'>
+                    <h2>Featured Products</h2>
+                    <div>
+                        {/* Featured products will be loaded here */}
+                        <p>Loading featured products...</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
